5f94a931a47a1cdaa1fc1fde5ed0775ada59ae52
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-01-17 15:18:14 +0300

    create popup window

--- app/src/main/java/com/pashikhmin/ismobileapp/map/FacilityInfoWindowAdapter.java
+++ app/src/main/java/com/pashikhmin/ismobileapp/map/FacilityInfoWindowAdapter.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import com.example.ismobileapp.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
+import com.pashikhmin.ismobileapp.model.Facility;
 
 public class FacilityInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     private Activity activity;
@@ -17,11 +21,18 @@ public class FacilityInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
     @Override
     public View getInfoWindow(Marker marker) {
-        return activity.getLayoutInflater().inflate(R.layout.layout_facility_brief, null, false);
+        return null;
     }
 
     @Override
     public View getInfoContents(Marker marker) {
-        return null;
+        Facility associatedFacility = ((Facility) marker.getTag());
+        View ret = activity.getLayoutInflater()
+                .inflate(R.layout.layout_facility_map_popup, null, false);
+        ((TextView) ret.findViewById(R.id.title)).setText(associatedFacility.getName());
+        ((TextView) ret.findViewById(R.id.facility_region)).setText(associatedFacility.getRegion().getTitle());
+        if (associatedFacility.getImage() != null)
+            ((ImageView) ret.findViewById(R.id.entity_image)).setImageDrawable(associatedFacility.getImage());
+        return ret;
     }
 }
