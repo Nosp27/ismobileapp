47ce8941c775c64b59c29c921527ff39e96406c0
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-02-14 22:39:53 +0300

    Helpdesk. create files.

---
+++ app/src/main/java/com/pashikhmin/ismobileapp/HelpDeskActivity.java
@@ -0,0 +1,14 @@
+package com.pashikhmin.ismobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ismobileapp.R;
+
+public class HelpDeskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_desk);
+    }
+}
