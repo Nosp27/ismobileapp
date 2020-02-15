597b060d6272656a4dc238499e2355047ff3381b
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-02-15 18:14:41 +0300

    Load messages and issues in HelpDeskActivity

--- app/src/main/java/com/pashikhmin/ismobileapp/HelpDeskActivity.java
+++ app/src/main/java/com/pashikhmin/ismobileapp/HelpDeskActivity.java
@@ -3,17 +3,20 @@ package com.pashikhmin.ismobileapp;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.ismobileapp.R;
 import com.pashikhmin.ismobileapp.model.Criteries;
+import com.pashikhmin.ismobileapp.model.helpdesk.Actor;
 import com.pashikhmin.ismobileapp.model.helpdesk.Issue;
+import com.pashikhmin.ismobileapp.model.helpdesk.Message;
 import com.pashikhmin.ismobileapp.network.Connectors;
 import com.pashikhmin.ismobileapp.network.loadTask.LoadTask;
 import com.pashikhmin.ismobileapp.network.loadTask.LoadTaskResult;
 import com.pashikhmin.ismobileapp.resourceSupplier.HelpDeskResourceSupplier;
-import com.pashikhmin.ismobileapp.resourceSupplier.ResourceSupplier;
+import com.pashikhmin.ismobileapp.viewmodel.IssueListAdapter;
 
 import java.io.IOException;
 import java.util.List;
@@ -44,22 +47,63 @@ public class HelpDeskActivity extends AppCompatActivity {
         }
     }
 
-    private void onIssuesLoaded(LoadTaskResult<List<Issue>> taskResult) {
-        if(!taskResult.successful()) {
-            setContentView(R.layout.error_screen);
-            findViewById(R.id.retry_button).setVisibility(View.INVISIBLE);
-            return;
-        }
-        List<Issue> result = taskResult.getResult();
-        LinearLayout messageContainer = findViewById(R.id.helpdesk_messages);
-        setContentView(R.layout.help_desk);
-        for(Issue issue : result) {
-            View messageView = getLayoutInflater().inflate(R.layout.help_desk_message_left, messageContainer);
-            fillIssueListItem(messageView, issue);
+    private List<Message> loadMessages(Issue issue, Criteries... criteries) {
+        try {
+            List<Message> messages = resourceSupplier.getIssueHistory(issue);
+            Actor me = resourceSupplier.finger();
+            for (Message message : messages)
+                message.setMine(me.getId() == message.getSenderId());
+            return messages;
+        } catch (IOException e) {
+            Log.e(TAG, "Exception while loading message history", e);
+            throw new RuntimeException(e);
         }
     }
 
-    private void fillIssueListItem(View listItem, Issue issue) {
-        ((TextView)listItem.findViewById(R.id.bubble)).setText(issue.getTopic());
+    private void onMessagesLoaded(LoadTaskResult<List<Message>> taskResult) {
+        if (!taskResult.successful()) {
+            processTaskFail();
+            return;
+        }
+        setContentView(R.layout.help_desk_messages);
+        List<Message> result = taskResult.getResult();
+        LinearLayout messageContainer = findViewById(R.id.helpdesk_messages);
+        for (Message message : result) {
+            View messageView = getLayoutInflater().inflate(
+                    message.isMine() ? R.layout.help_desk_message_right : R.layout.help_desk_message_left, null
+            );
+            messageContainer.addView(messageView);
+            fillMessageListItem(messageView, message);
+        }
+    }
+
+    private void onIssuesLoaded(LoadTaskResult<List<Issue>> taskResult) {
+        if (!taskResult.successful()) {
+            processTaskFail();
+            return;
+        }
+        setContentView(R.layout.help_desk_issues);
+        List<Issue> result = taskResult.getResult();
+        ListView issueListView = findViewById(R.id.helpdesk_issues);
+        issueListView.setAdapter(
+                new IssueListAdapter(this, R.layout.helpdesk_issue, result, this::onIssueClick)
+        );
+    }
+
+    private void onIssueClick(Issue clicked) {
+        setContentView(R.layout.layout_loading);
+        LoadTask<List<Message>> loadMessagesTask = new LoadTask<>(
+                e -> loadMessages(clicked, e), this::onMessagesLoaded
+        );
+        loadMessagesTask.execute();
+    }
+
+    private void fillMessageListItem(View listItem, Message message) {
+        ((TextView) listItem.findViewById(R.id.bubble)).setText(message.getContent());
+    }
+
+    private void processTaskFail() {
+        setContentView(R.layout.error_screen);
+        findViewById(R.id.retry_button).setVisibility(View.INVISIBLE);
     }
 }
--- app/src/main/java/com/pashikhmin/ismobileapp/model/helpdesk/Message.java
+++ app/src/main/java/com/pashikhmin/ismobileapp/model/helpdesk/Message.java
@@ -2,8 +2,61 @@ package com.pashikhmin.ismobileapp.model.helpdesk;
 
 import com.pashikhmin.ismobileapp.network.json.JSONField;
 
-public class Message {
-    int id;
+import java.io.Serializable;
+
+public class Message implements Serializable {
     @JSONField
-    String content;
+    private int id;
+    @JSONField
+    private String content;
+    @JSONField
+    private Actor sender;
+    @JSONField
+    private Issue issue;
+
+    private transient Boolean isMine;
+
+    public Message() {
+    }
+
+    public Message(String content, Actor sender) {
+        this.content = content;
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getSenderId() {
+        return sender.getId();
+    }
+
+    public Actor getSender() {
+        return sender;
+    }
+
+    public void setSender(Actor sender) {
+        this.sender = sender;
+    }
+
+    public Issue getIssue() {
+        return issue;
+    }
+
+    public void setIssue(Issue issue) {
+        this.issue = issue;
+    }
+
+    public Boolean isMine() {
+        return isMine;
+    }
+
+    public void setMine(Boolean mine) {
+        isMine = mine;
+    }
 }
