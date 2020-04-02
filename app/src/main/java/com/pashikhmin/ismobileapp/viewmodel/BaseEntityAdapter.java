7f2e6897edac709831921b96c0adf0e7744a1130
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-04-02 13:47:52 +0300

    Fix facility subtitle
    Region name instead of coordinates
    
    Fixup subtitle commit

--- app/src/main/java/com/pashikhmin/ismobileapp/viewmodel/BaseEntityAdapter.java
+++ app/src/main/java/com/pashikhmin/ismobileapp/viewmodel/BaseEntityAdapter.java
@@ -38,6 +38,10 @@ public abstract class BaseEntityAdapter extends ArrayAdapter<Entity> {
         Entity entity = entities.get(position);
 
         ((TextView) convertView.findViewById(R.id.list_title)).setText(entity.getTitle());
+
+        TextView subtitle = convertView.findViewById(R.id.region_title);
+        if (subtitle != null)
+            subtitle.setText(entity.getSubtitle());
         if (entity.getImage() != null)
             ((ImageView) convertView.findViewById(R.id.item_img)).setImageDrawable(entity.getImage());
 
