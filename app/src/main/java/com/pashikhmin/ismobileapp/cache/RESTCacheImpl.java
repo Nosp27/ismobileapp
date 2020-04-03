7bd5c6571341a02213e7d2d84fa9f9a849e40c2b
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-04-03 11:33:50 +0300

    Use cache matchers

--- app/src/main/java/com/pashikhmin/ismobileapp/cache/RESTCacheImpl.java
+++ app/src/main/java/com/pashikhmin/ismobileapp/cache/RESTCacheImpl.java
@@ -1,6 +1,7 @@
 package com.pashikhmin.ismobileapp.cache;
 
 import com.pashikhmin.ismobileapp.network.connectors.RESTConnector;
+import org.hamcrest.Matcher;
 import org.json.JSONTokener;
 
 import java.io.IOException;
@@ -11,15 +12,28 @@ public class RESTCacheImpl implements RESTConnector {
     private RESTConnector underlying;
     private Map<String, String> cacheMap;
 
-    public RESTCacheImpl(RESTConnector underlying) {
+    private Matcher<String> getMatcher;
+    private Matcher<String> postMatcher;
+
+    private CacheWarmer warmer;
+
+    public RESTCacheImpl(RESTConnector underlying, CacheBuilder builder) {
+        if (builder != null) {
+            getMatcher = builder.getMatcher;
+            postMatcher = builder.postMatcher;
+            warmer = builder.warmer;
+        }
+
         cacheMap = new HashMap<>();
         this.underlying = underlying;
     }
 
     @Override
     public JSONTokener get(String path) throws IOException {
+        if (!getMatcher.matches(path))
+            return new JSONTokener(underlying.get(path).nextTo((char) 0));
         if (!cacheMap.containsKey(path))
-            cacheMap.put(path, underlying.get(path).nextTo((char)0));
+            cacheMap.put(path, underlying.get(path).nextTo((char) 0));
         return new JSONTokener(cacheMap.get(path));
     }
 
